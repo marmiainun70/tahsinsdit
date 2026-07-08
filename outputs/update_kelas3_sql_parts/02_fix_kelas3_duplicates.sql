581fcac1d23d721cb1b3b10f69ac28d322947598
4rsyad4asri <4rsyad4asri@users.noreply.github.com>
2026-07-08 17:26:10 +0700

    fix: explicit sql to clean duplicate names without losing NIS

---
+++ outputs/update_kelas3_sql_parts/02_fix_kelas3_duplicates.sql
@@ -0,0 +1,88 @@
+DO $$
+BEGIN
+
+    -- Perbaiki Adzkia Salsabila Darma
+    DELETE FROM students WHERE nama = 'Adzkiya Salsabila Darma' AND nis IS NULL AND kelas = 3;
+    UPDATE students SET nama = 'Adzkiya Salsabila Darma', kelas = 3, rombel = 'D' WHERE nama = 'Adzkia Salsabila Darma';
+
+    -- Perbaiki Aisyah Al-Syafiyyah
+    DELETE FROM students WHERE nama = 'Aisyah Al-Shafiyyah' AND nis IS NULL AND kelas = 3;
+    UPDATE students SET nama = 'Aisyah Al-Shafiyyah', kelas = 3, rombel = 'D' WHERE nama = 'Aisyah Al-Syafiyyah';
+
+    -- Perbaiki Nadya Carissa Pane
+    DELETE FROM students WHERE nama = 'Nadya Carisssa Pane' AND nis IS NULL AND kelas = 3;
+    UPDATE students SET nama = 'Nadya Carisssa Pane', kelas = 3, rombel = 'C' WHERE nama = 'Nadya Carissa Pane';
+
+    -- Perbaiki Nusaibah Humaroh
+    DELETE FROM students WHERE nama = 'Nusaibah Humairoh' AND nis IS NULL AND kelas = 3;
+    UPDATE students SET nama = 'Nusaibah Humairoh', kelas = 3, rombel = 'D' WHERE nama = 'Nusaibah Humaroh';
+
+    -- Perbaiki Alzafran Raffasya Prastio
+    DELETE FROM students WHERE nama = 'Alzhafran Raffasya Prastiyo' AND nis IS NULL AND kelas = 3;
+    UPDATE students SET nama = 'Alzhafran Raffasya Prastiyo', kelas = 3, rombel = 'A' WHERE nama = 'Alzafran Raffasya Prastio';
+
+    -- Perbaiki Arelya Sandra
+    DELETE FROM students WHERE nama = 'Arelya Shandra' AND nis IS NULL AND kelas = 3;
+    UPDATE students SET nama = 'Arelya Shandra', kelas = 3, rombel = 'C' WHERE nama = 'Arelya Sandra';
+
+    -- Perbaiki Elifia Silfany Putri
+    DELETE FROM students WHERE nama = 'Elifia Sylvani Putri' AND nis IS NULL AND kelas = 3;
+    UPDATE students SET nama = 'Elifia Sylvani Putri', kelas = 3, rombel = 'C' WHERE nama = 'Elifia Silfany Putri';
+
+    -- Perbaiki Hanan Abiyyu Harahap
+    DELETE FROM students WHERE nama = 'Hanan Abiyyu' AND nis IS NULL AND kelas = 3;
+    UPDATE students SET nama = 'Hanan Abiyyu', kelas = 3, rombel = 'A' WHERE nama = 'Hanan Abiyyu Harahap';
+
+    -- Perbaiki Muhammad Alfatih Bangun
+    DELETE FROM students WHERE nama = 'Muhammad Al-Fatih Bangun' AND nis IS NULL AND kelas = 3;
+    UPDATE students SET nama = 'Muhammad Al-Fatih Bangun', kelas = 3, rombel = 'A' WHERE nama = 'Muhammad Alfatih Bangun';
+
+    -- Perbaiki Muhammad Irham Atallah
+    DELETE FROM students WHERE nama = 'Muhammad Irham Athallah' AND nis IS NULL AND kelas = 3;
+    UPDATE students SET nama = 'Muhammad Irham Athallah', kelas = 3, rombel = 'A' WHERE nama = 'Muhammad Irham Atallah';
+
+    -- Perbaiki Nuqman Firadaus
+    DELETE FROM students WHERE nama = 'Nuqman Firdaus' AND nis IS NULL AND kelas = 3;
+    UPDATE students SET nama = 'Nuqman Firdaus', kelas = 3, rombel = 'B' WHERE nama = 'Nuqman Firadaus';
+
+    -- Perbaiki Nurhanifah Hafsah Hafman
+    DELETE FROM students WHERE nama = 'Nur Hanifah Hafsah Hafman' AND nis IS NULL AND kelas = 3;
+    UPDATE students SET nama = 'Nur Hanifah Hafsah Hafman', kelas = 3, rombel = 'D' WHERE nama = 'Nurhanifah Hafsah Hafman';
+
+    -- Perbaiki Rayga Prasetiyo
+    DELETE FROM students WHERE nama = 'Rayga Prasetyo' AND nis IS NULL AND kelas = 3;
+    UPDATE students SET nama = 'Rayga Prasetyo', kelas = 3, rombel = 'B' WHERE nama = 'Rayga Prasetiyo';
+
+    -- Perbaiki Razania Almasyirah Surbakti
+    DELETE FROM students WHERE nama = 'Razania Almashyra Surbakti' AND nis IS NULL AND kelas = 3;
+    UPDATE students SET nama = 'Razania Almashyra Surbakti', kelas = 3, rombel = 'D' WHERE nama = 'Razania Almasyirah Surbakti';
+
+    -- Perbaiki Abdurrahman Hanif As Shiddiq
+    DELETE FROM students WHERE nama = 'Abdurrahman Hanif Ash-Shiddiq' AND nis IS NULL AND kelas = 3;
+    UPDATE students SET nama = 'Abdurrahman Hanif Ash-Shiddiq', kelas = 3, rombel = 'B' WHERE nama = 'Abdurrahman Hanif As Shiddiq';
+
+    -- Perbaiki Fatir Akaria
+    DELETE FROM students WHERE nama = 'Fatir Zakaria' AND nis IS NULL AND kelas = 3;
+    UPDATE students SET nama = 'Fatir Zakaria', kelas = 3, rombel = 'B' WHERE nama = 'Fatir Akaria';
+
+    -- Perbaiki M. Yusuf Alfatih
+    DELETE FROM students WHERE nama = 'M.Yusuf Alfatih' AND nis IS NULL AND kelas = 3;
+    UPDATE students SET nama = 'M.Yusuf Alfatih', kelas = 3, rombel = 'A' WHERE nama = 'M. Yusuf Alfatih';
+
+    -- Perbaiki Abdullah Abrar Taqy Al Sudar
+    DELETE FROM students WHERE nama = 'Abdullah Abrar Taqiy Al Sudar' AND nis IS NULL AND kelas = 3;
+    UPDATE students SET nama = 'Abdullah Abrar Taqiy Al Sudar', kelas = 3, rombel = 'A' WHERE nama = 'Abdullah Abrar Taqy Al Sudar';
+
+    -- Perbaiki Ustman Ibnu Affan
+    DELETE FROM students WHERE nama = 'Utsman Ibnu Affan' AND nis IS NULL AND kelas = 3;
+    UPDATE students SET nama = 'Utsman Ibnu Affan', kelas = 3, rombel = 'A' WHERE nama = 'Ustman Ibnu Affan';
+
+    -- Perbaiki Mhd. Al Faiz Harahap
+    DELETE FROM students WHERE nama = 'Mhd Al Faiz Harahap' AND nis IS NULL AND kelas = 3;
+    UPDATE students SET nama = 'Mhd Al Faiz Harahap', kelas = 3, rombel = 'A' WHERE nama = 'Mhd. Al Faiz Harahap';
+
+    -- Perbaiki Abrizam Abde Habibie
+    DELETE FROM students WHERE nama = 'Abrisam Abde Habibie' AND nis IS NULL AND kelas = 3;
+    UPDATE students SET nama = 'Abrisam Abde Habibie', kelas = 3, rombel = 'B' WHERE nama = 'Abrizam Abde Habibie';
+
+END $$;
